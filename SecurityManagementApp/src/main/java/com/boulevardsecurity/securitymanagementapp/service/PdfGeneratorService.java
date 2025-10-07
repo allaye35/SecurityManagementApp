f1533c554f891800d702742dc8c3a98cfb1de044
allaye <allaye35@gmail.com>
2025-10-07 16:34:49 +0200

    Nettoyage: suppression fichiers inutiles (logo.svg, PdfGenerator, bin/)

--- SecurityManagementApp/src/main/java/com/boulevardsecurity/securitymanagementapp/service/PdfGeneratorService.java
+++
@@ -1,5 +0,0 @@
-//package com.boulevardsecurity.securitymanagementapp.service;
-//import com.boulevardsecurity.securitymanagementapp.model.*;
-//import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
-//import org.springframework.stereotype.Service;
-//import java.io.ByteArrayOutputStream;
